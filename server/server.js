177cbf3124ec972b1ed62c481617d096dd8621f3
jacob nordan <jacob.nordan113322@gmail.com>
2022-01-10 16:21:42 -0800

    adding server auth

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '14bdf0a65f924939bbd0cfc5e28708b9',
+        clientSecret: '46e520fc6c6f4fbdb26bb7368b4cfc76'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001)
